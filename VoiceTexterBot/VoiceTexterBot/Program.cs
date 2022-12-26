5a9428ba3bb4394a94c9d9a08439d33f1bf3562d
Ilya <ilyakhodakov23@gmail.com>
2022-12-26 09:31:44 +0300

    VoiceTexterBot

---
+++ VoiceTexterBot/VoiceTexterBot/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Text;
+using Telegram.Bot;
+using VoiceTexterBot.Configuration;
+using VoiceTexterBot.Controllers;
+using VoiceTexterBot.Services;
+
+namespace VoiceTexterBot
+{
+    internal class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // Объект, отвечающий за постоянный жизненный цикл приложения
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+                .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+                .Build(); // Собираем
+
+            Console.WriteLine("Сервис запущен");
+            // Запускаем сервис
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            //И обновим метод ConfigureServices(...), добавив инициализацию конфигурации в его начало:
+            AppSettings appSettings = BuildAppSettings();
+            services.AddSingleton(BuildAppSettings());
+            //Для того чтобы сделать хранилище сессий доступным всем компонентам приложения, его нужно добавить в контейнер зависимостей:
+            services.AddSingleton<IStorage, MemoryStorage>();
+            // Подключаем контроллеры сообщений и кнопок
+            services.AddTransient<DefaultMessageController>();
+            services.AddTransient<VoiceMessageController>();
+            services.AddTransient<TextMessageController>();
+            services.AddTransient<InlineKeyboardController>();
+            // Регистрируем объект TelegramBotClient c токеном подключения
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("5859120837:AAGLzs6dhvK-4arrx4O_vulUlZRmZEC9FPg"));
+            // Регистрируем постоянно активный сервис бота
+            services.AddHostedService<Bot>(); 
+        }
+
+        static AppSettings BuildAppSettings()
+        {
+            return new AppSettings()
+            {
+                BotToken = "5859120837:AAGLzs6dhvK-4arrx4O_vulUlZRmZEC9FPg"
+            };
+        }
+    }
+}
---
+++ VoiceTexterBot/VoiceTexterBot/Services/IStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoiceTexterBot.Models;
+
+namespace VoiceTexterBot.Services
+{
+    internal interface IStorage
+    {
+        // <summary>
+        /// Получение сессии пользователя по идентификатору
+        /// </summary>
+        Session GetSession(long chatId);
+    }
+}
